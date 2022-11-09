c278c10b855994e39bc707db60ae7b75983a1003
Felipe Tavares <xfelipetavares@gmail.com>
2022-11-09 10:44:11 -0300

    eslint added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'
 
-const passwordButton = document.querySelector('button');
-const displayPW = document.querySelector('h2');
+const passwordButton = document.querySelector('button')
+const displayPW = document.querySelector('h2')
 
 passwordButton.addEventListener('click', () => {
-  const password = nanoid();
-  displayPW.innerText = password;
-});
+  const password = nanoid()
+  displayPW.innerText = password
+})
